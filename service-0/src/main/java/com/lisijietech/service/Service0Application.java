cd02ede1301c6da27d4ed08e93739abbd76e2d7c
lisijie <414024639@qq.com>
2020-12-01 02:24:50 +0800

    Spring-Cloud学习

---
+++ service-0/src/main/java/com/lisijietech/service/Service0Application.java
@@ -0,0 +1,13 @@
+package com.lisijietech.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service0Application.class, args);
+	}
+
+}
---
+++ service-0/src/test/java/com/lisijietech/service/Service0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisijietech.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
